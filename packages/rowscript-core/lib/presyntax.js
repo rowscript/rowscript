309106a194e188cd61acd8736689578d156c9bb1
anqurvanillapy <anqurvanillapy@gmail.com>
2021-08-05 18:36:01 +0800

    Init presyntax

---
+++ packages/rowscript-core/lib/presyntax.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export class Env {
+  constructor() {
+    this._wellTyped = {}
+  }
+}
+
+export class Ctx {
+  constructor() {
+    this._satisfied = {}
+  }
+}
+
+export class Term {}
