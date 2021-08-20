856f7743758a86124bb729ab9488d452ed6aba67
anqurvanillapy <anqurvanillapy@gmail.com>
2021-08-20 18:38:45 +0800

    More grammar

--- packages/tree-sitter-rowscript/grammar.js
+++ packages/tree-sitter-rowscript/grammar.js
@@ -197,10 +197,9 @@ export default grammar({
         $.parenthesizedExpression,
         $.identifier,
         $.this,
-        // TODO: Design of `super`.
+        $.super,
         $.number,
         $.string,
-        $.templateString,
         $.regex,
         $.false,
         $.true,
@@ -299,12 +298,146 @@ export default grammar({
 
     _propertyName: $ => choice($.identifier, $.string, $.number),
 
+    object: $ =>
+      prec(
+        'object',
+        seq(
+          '{',
+          optional(
+            commaSep(optional(choice($.pair, $.methodDefinition, $.identifier)))
+          ),
+          '}'
+        )
+      ),
+
+    pair: $ =>
+      seq(field('key', $._propertyName), ':', field('value', $.expression)),
+
+    array: $ => seq('[', commaSep(optional($.expression)), ']'),
+
+    arrowFunction: $ =>
+      seq(
+        choice(field('parameter', $.identifier), $.declarationSignature),
+        '=>',
+        field('body', choice($.expression, $.statementBlock))
+      ),
+
     identifier: $ => token(seq(ALPHA, repeat(ALNUM))),
 
     this: $ => 'this',
     super: $ => 'super',
     true: $ => 'true',
     false: $ => 'false',
+
+    number: $ => {
+      const hexLiteral = seq(choice('0x', '0X'), /[\da-fA-F](_?[\da-fA-F])*/)
+
+      const decimalDigits = /\d(_?\d)*/
+      const signedInteger = seq(optional(choice('-', '+')), decimalDigits)
+      const exponentPart = seq(choice('e', 'E'), signedInteger)
+
+      const binaryLiteral = seq(choice('0b', '0B'), /[0-1](_?[0-1])*/)
+
+      const octalLiteral = seq(choice('0o', '0O'), /[0-7](_?[0-7])*/)
+
+      const bigintLiteral = seq(
+        choice(hexLiteral, binaryLiteral, octalLiteral, decimalDigits),
+        'n'
+      )
+
+      const decimalIntegerLiteral = choice(
+        '0',
+        seq(optional('0'), /[1-9]/, optional(seq(optional('_'), decimalDigits)))
+      )
+
+      const decimalLiteral = choice(
+        seq(
+          decimalIntegerLiteral,
+          '.',
+          optional(decimalDigits),
+          optional(exponentPart)
+        ),
+        seq('.', decimalDigits, optional(exponentPart)),
+        seq(decimalIntegerLiteral, exponentPart),
+        seq(decimalDigits)
+      )
+
+      return token(
+        choice(
+          hexLiteral,
+          decimalLiteral,
+          binaryLiteral,
+          octalLiteral,
+          bigintLiteral
+        )
+      )
+    },
+
+    string: $ =>
+      choice(
+        seq(
+          '"',
+          repeat(
+            choice(
+              alias($.unescapedDoubleStringFragment, $.stringFragment),
+              $.escapeSequence
+            )
+          ),
+          '"'
+        ),
+        seq(
+          "'",
+          repeat(
+            choice(
+              alias($.unescapedSingleStringFragment, $.stringFragment),
+              $.escapeSequence
+            )
+          ),
+          "'"
+        )
+      ),
+
+    unescapedDoubleStringFragment: $ => token.immediate(prec(1, /[^"\\]+/)),
+
+    unescapedSingleStringFragment: $ => token.immediate(prec(1, /[^'\\]+/)),
+
+    escapeSequence: $ =>
+      token.immediate(
+        seq(
+          '\\',
+          choice(
+            /[^xu0-7]/,
+            /[0-7]{1,3}/,
+            /x[0-9a-fA-F]{2}/,
+            /u[0-9a-fA-F]{4}/,
+            /u{[0-9a-fA-F]+}/
+          )
+        )
+      ),
+
+    regex: $ =>
+      seq(
+        '/',
+        field('pattern', $.regexPattern),
+        token.immediate('/'),
+        optional(field('flags', $.regexFlags))
+      ),
+
+    regexPattern: $ =>
+      token.immediate(
+        prec(
+          -1,
+          repeat1(
+            choice(
+              seq('[', repeat(choice(seq('\\', /./), /[^\]\n\\]/)), ']'),
+              seq('\\', /./),
+              /[^/\\\[\n]/
+            )
+          )
+        )
+      ),
+
+    regexFlags: $ => token.immediate(/[a-z]+/),
 
     comment: $ =>
       token(choice(seq('//', /.*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/')))
