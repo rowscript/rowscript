d1fa47a1e4dd388612067084c14bb30204e4a01d
anqurvanillapy <anqurvanillapy@gmail.com>
2021-09-02 19:19:20 +0800

    Fix dependencies

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,5 +1,12 @@
 'use strict'
 
+import Parser from 'tree-sitter'
+import RowScript from '@rowscript/tree-sitter'
+
 export default args => {
   console.log(args)
+  const parser = new Parser()
+  parser.setLanguage(RowScript)
+  const tree = parser.parse(`function a() {}`)
+  console.log(tree)
 }
