da04e2242d8ef6ecfd18036b4a4a2f9b5bfbcb4e
anqurvanillapy <anqurvanillapy@gmail.com>
2021-10-07 03:21:58 +0800

    Try some prec_climber huh

--- packages/rowscript-core/src/surf/parsing.rs
+++ packages/rowscript-core/src/surf/parsing.rs
@@ -1,8 +1,32 @@
+use pest::prec_climber::{Assoc, Operator, PrecClimber};
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "surf/grammar.pest"]
 struct Surf;
+
+#[allow(non_camel_case_types)]
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Rule {
+    program,
+
+    primary_expression,
+    binary_expression,
+    ternary_expression,
+    subscript_expression,
+    member_expression,
+    call_expression,
+}
+
+static PREC_CLIMBER: PrecClimber<Rule> = PrecClimber::new(vec![
+    Operator::new(Rule::primary_expression, Assoc::Left),
+    Operator::new(Rule::call_expression, Assoc::Left),
+    Operator::new(Rule::member_expression, Assoc::Left)
+        | Operator::new(Rule::subscript_expression, Assoc::Left),
+    Operator::new(Rule::ternary_expression, Assoc::Right),
+    Operator::new(Rule::binary_expression, Assoc::Left),
+]);
 
 fn parse() {
     // TODO
