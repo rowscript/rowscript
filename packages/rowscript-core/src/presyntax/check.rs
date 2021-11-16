bd10a755f90a7cf24a8758e5df9cb9246ce3d546
anqurvanillapy <anqurvanillapy@gmail.com>
2021-11-16 18:20:33 +0800

    More tiny insights about tycking

--- packages/rowscript-core/src/presyntax/check.rs
+++ packages/rowscript-core/src/presyntax/check.rs
@@ -1,7 +1,9 @@
-use crate::basis::data::{Ident, Lvl};
-use crate::presyntax::data::Term;
-use crate::syntax::data as V;
+use crate::basis::data::{Ident, Ix, Lvl};
+use crate::presyntax::check::CheckError::NameNotInScope;
+use crate::presyntax::data::Term::Var;
+use crate::presyntax::data::{Term, Type};
 use std::collections::HashMap;
+use std::process::id;
 use tree_sitter::Point;
 
 #[derive(Debug)]
@@ -17,10 +19,29 @@ enum BD {
 }
 
 #[derive(Debug, Default)]
+struct Env {
+    env: Vec<(String, Term)>,
+}
+
+impl Env {
+    fn resolve_name(&self, var: &mut Term) -> Result<(), CheckError> {
+        if let Var(ident, ix) = var {
+            for i in 0..self.env.len() {
+                if ident.text == self.env[i].0 {
+                    *ix = i as Ix
+                }
+            }
+            return Err(NameNotInScope(ident.to_owned()));
+        }
+        unreachable!()
+    }
+}
+
+#[derive(Debug, Default)]
 pub struct Ctx {
-    env: Vec<V::Term>,
+    env: Vec<Term>,
     lvl: Lvl,
-    raw_named: HashMap<String, V::Term>,
+    raw_named: Vec<(String, Term)>,
     bds: Vec<BD>,
     pt: Point,
 }
@@ -30,7 +51,24 @@ impl Ctx {
         Default::default()
     }
 
-    pub fn check(&mut self, tm: &mut Term) -> Result<(), CheckError> {
-        Ok(())
+    pub fn scope_check(&mut self, tm: &mut Term) -> Result<(), CheckError> {
+        todo!()
+    }
+
+    pub fn infer(&mut self, tm: Term) -> Result<Type, CheckError> {
+        use Term::*;
+        match tm {
+            Unit => Ok(Type::Unit),
+            Str(_) => Ok(Type::Str),
+            Num(_) => Ok(Type::Num),
+            Bool(_) => Ok(Type::Bool),
+            BigInt(_) => Ok(Type::BigInt),
+            Tuple(xs) => xs
+                .into_iter()
+                .map(|x| self.infer(x))
+                .collect::<Result<Vec<Type>, _>>()
+                .map(|ts| Type::Tuple(ts)),
+            _ => todo!(),
+        }
     }
 }
