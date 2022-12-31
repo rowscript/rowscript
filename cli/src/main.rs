f2a78d17f071f579c3a94d8e81657dc39cbb7cc8
anqurvanillapy <anqurvanillapy@gmail.com>
2022-12-31 21:02:18 +0800

    Simple command line UI
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+use clap::Parser;
+
+use rowscript_core::Driver;
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    #[arg(index = 1, default_value = ".")]
+    path: PathBuf,
+}
+
+fn main() -> ExitCode {
+    Driver::new(Args::parse().path)
+        .check()
+        .map_or(ExitCode::FAILURE, |_| ExitCode::SUCCESS)
 }
