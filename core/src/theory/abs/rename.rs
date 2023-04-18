9677d913b1c84bc11bd3e0a93af5b76d5a8e249d
anqurvanillapy <anqurvanillapy@gmail.com>
2023-04-18 17:46:41 +0800

    Unnecessary boxing in renaming
    Signed-off-by: anqurvanillapy <anqurvanillapy@gmail.com>

--- core/src/theory/abs/rename.rs
+++ core/src/theory/abs/rename.rs
@@ -7,60 +7,67 @@ use crate::theory::{Param, Var};
 pub struct Renamer(HashMap<Var, Var>);
 
 impl Renamer {
-    pub fn term(&mut self, tm: Term) -> Box<Term> {
+    pub fn term(&mut self, tm: Term) -> Term {
         use Term::*;
-        Box::new(match tm {
+        match tm {
             Ref(x) => self.0.get(&x).map_or(Ref(x), |y| Ref(y.clone())),
             MetaRef(k, r, sp) => MetaRef(
                 k,
                 r,
-                sp.into_iter().map(|(i, tm)| (i, *self.term(tm))).collect(),
+                sp.into_iter().map(|(i, tm)| (i, self.term(tm))).collect(),
             ),
             Let(p, a, b) => {
                 let a = self.term(*a); // not guarded by `p`, rename it first
-                Let(self.param(p), a, self.term(*b))
+                Let(self.param(p), Box::new(a), Box::new(self.term(*b)))
             }
-            Pi(p, c) => Pi(self.param(p), self.term(*c)),
-            Lam(p, b) => Lam(self.param(p), self.term(*b)),
-            App(f, i, x) => App(self.term(*f), i, self.term(*x)),
-            Sigma(p, c) => Sigma(self.param(p), self.term(*c)),
-            Tuple(a, b) => Tuple(self.term(*a), self.term(*b)),
-            TupleLet(x, y, a, b) => {
-                TupleLet(self.param(x), self.param(y), self.term(*a), self.term(*b))
-            }
-            UnitLet(a, b) => UnitLet(self.term(*a), self.term(*b)),
-            If(p, t, e) => If(self.term(*p), self.term(*t), self.term(*e)),
+            Pi(p, c) => Pi(self.param(p), Box::new(self.term(*c))),
+            Lam(p, b) => Lam(self.param(p), Box::new(self.term(*b))),
+            App(f, i, x) => App(Box::new(self.term(*f)), i, Box::new(self.term(*x))),
+            Sigma(p, c) => Sigma(self.param(p), Box::new(self.term(*c))),
+            Tuple(a, b) => Tuple(Box::new(self.term(*a)), Box::new(self.term(*b))),
+            TupleLet(x, y, a, b) => TupleLet(
+                self.param(x),
+                self.param(y),
+                Box::new(self.term(*a)),
+                Box::new(self.term(*b)),
+            ),
+            UnitLet(a, b) => UnitLet(Box::new(self.term(*a)), Box::new(self.term(*b))),
+            If(p, t, e) => If(
+                Box::new(self.term(*p)),
+                Box::new(self.term(*t)),
+                Box::new(self.term(*e)),
+            ),
             Fields(fields) => {
                 let mut m = FieldMap::default();
                 for (f, tm) in fields {
-                    m.insert(f, *self.term(tm));
+                    m.insert(f, self.term(tm));
                 }
                 Fields(m)
             }
-            Combine(a, b) => Combine(self.term(*a), self.term(*b)),
-            RowOrd(a, d, b) => RowOrd(self.term(*a), d, self.term(*b)),
-            RowEq(a, b) => RowEq(self.term(*a), self.term(*b)),
-            Object(f) => Object(self.term(*f)),
-            Obj(f) => Obj(self.term(*f)),
-            Concat(a, b) => Concat(self.term(*a), self.term(*b)),
-            Access(a, n) => Access(self.term(*a), n),
-            Downcast(a, f) => Downcast(self.term(*a), self.term(*f)),
-            Enum(f) => Enum(self.term(*f)),
-            Variant(f) => Variant(self.term(*f)),
-            Upcast(a, f) => Upcast(self.term(*a), self.term(*f)),
+            Combine(a, b) => Combine(Box::new(self.term(*a)), Box::new(self.term(*b))),
+            RowOrd(a, d, b) => RowOrd(Box::new(self.term(*a)), d, Box::new(self.term(*b))),
+            RowEq(a, b) => RowEq(Box::new(self.term(*a)), Box::new(self.term(*b))),
+            Object(f) => Object(Box::new(self.term(*f))),
+            Obj(f) => Obj(Box::new(self.term(*f))),
+            Concat(a, b) => Concat(Box::new(self.term(*a)), Box::new(self.term(*b))),
+            Access(a, n) => Access(Box::new(self.term(*a)), n),
+            Downcast(a, f) => Downcast(Box::new(self.term(*a)), Box::new(self.term(*f))),
+            Enum(f) => Enum(Box::new(self.term(*f))),
+            Variant(f) => Variant(Box::new(self.term(*f))),
+            Upcast(a, f) => Upcast(Box::new(self.term(*a)), Box::new(self.term(*f))),
             Switch(a, cs) => {
                 let a = self.term(*a);
                 let mut m = CaseMap::default();
                 for (n, (v, tm)) in cs {
-                    m.insert(n, (v, *self.term(tm)));
+                    m.insert(n, (v, self.term(tm)));
                 }
-                Switch(a, m)
+                Switch(Box::new(a), m)
             }
-            Vptr(r, ts) => Vptr(r, ts.into_iter().map(|t| *self.term(t)).collect()),
-            Vp(r, ts) => Vp(r, ts.into_iter().map(|t| *self.term(t)).collect()),
-            Lookup(a) => Lookup(self.term(*a)),
-            Find(ty, i, f) => Find(self.term(*ty), i, f),
-            ImplementsOf(a, i) => ImplementsOf(self.term(*a), i),
+            Vptr(r, ts) => Vptr(r, ts.into_iter().map(|t| self.term(t)).collect()),
+            Vp(r, ts) => Vp(r, ts.into_iter().map(|t| self.term(t)).collect()),
+            Lookup(a) => Lookup(Box::new(self.term(*a))),
+            Find(ty, i, f) => Find(Box::new(self.term(*ty)), i, f),
+            ImplementsOf(a, i) => ImplementsOf(Box::new(self.term(*a)), i),
 
             Extern(r) => Extern(r),
             Undef(r) => Undef(r),
@@ -80,7 +87,7 @@ impl Renamer {
             RowSat => RowSat,
             RowRefl => RowRefl,
             ImplementsSat => ImplementsSat,
-        })
+        }
     }
 
     fn param(&mut self, p: Param<Term>) -> Param<Term> {
@@ -89,11 +96,11 @@ impl Renamer {
         Param {
             var,
             info: p.info,
-            typ: self.term(*p.typ),
+            typ: Box::new(self.term(*p.typ)),
         }
     }
 }
 
-pub fn rename(tm: Term) -> Box<Term> {
+pub fn rename(tm: Term) -> Term {
     Renamer::default().term(tm)
 }
