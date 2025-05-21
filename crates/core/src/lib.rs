460dccfdc3687b846e3e440f3811dbe1c25f5dc7
Anqur Lu <anqurvanillapy@gmail.com>
2025-05-21 10:37:53 +0800

    Basic example

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,12 +3,4 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests;
