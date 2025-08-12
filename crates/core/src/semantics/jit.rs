6ae10313e56046804ebefe5acac4aa89063782a4
Anqur Lu <anqurvanillapy@gmail.com>
2025-08-12 17:38:01 +0800

    JIT function call
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

--- crates/core/src/semantics/jit.rs
+++ crates/core/src/semantics/jit.rs
@@ -2,22 +2,24 @@ use cranelift::codegen::Context;
 use cranelift::prelude::settings::{Flags, builder as flags_builder};
 use cranelift::prelude::types::{F32, F64, I8, I16, I32, I64};
 use cranelift::prelude::{
-    AbiParam, Configurable, FunctionBuilder, FunctionBuilderContext, InstBuilder, Type as JitType,
-    Value, Variable,
+    AbiParam, Configurable, FunctionBuilder, FunctionBuilderContext, InstBuilder, Signature,
+    Type as JitType, Value, Variable,
 };
 use cranelift_jit::{JITBuilder, JITModule};
-use cranelift_module::{Module, default_libcall_names};
+use cranelift_module::{Linkage, Module, default_libcall_names};
 use cranelift_native::builder as native_builder;
 
-use crate::semantics::{BuiltinType, Func, Type};
+use crate::semantics::{BuiltinType, Func, FunctionType, Functions, Type};
 use crate::syntax::{Expr, Id, Ident};
 
-struct Jit {
+struct Jit<'a> {
+    fns: &'a Functions,
     m: JITModule,
+    ctx: Context,
 }
 
-impl Default for Jit {
-    fn default() -> Self {
+impl<'a> Jit<'a> {
+    fn new(fns: &'a Functions) -> Self {
         let mut flags = flags_builder();
         flags.set("use_colocated_libcalls", "false").unwrap();
         flags.set("is_pic", "false").unwrap();
@@ -25,49 +27,33 @@ impl Default for Jit {
             native_builder().unwrap().finish(Flags::new(flags)).unwrap(),
             default_libcall_names(),
         ));
-        Self { m }
+        let ctx = m.make_context();
+        Self { fns, m, ctx }
     }
-}
 
-impl Jit {
-    fn func<'a>(&'a self, id: &'a Id, f: &'a Func) -> JitFunc<'a> {
+    fn func(&'a mut self, id: &'a Id, f: &'a Func) -> JitFunc<'a> {
         JitFunc {
-            builder: Default::default(),
-            ctx: self.m.make_context(),
-            m: &self.m,
+            jit: self,
             id,
             f,
+            ctx: Default::default(),
         }
     }
 }
 
 struct JitFunc<'a> {
-    builder: FunctionBuilderContext,
-    ctx: Context,
-
-    m: &'a JITModule,
+    jit: &'a mut Jit<'a>,
     id: &'a Id,
     f: &'a Func,
+
+    ctx: FunctionBuilderContext,
 }
 
 impl<'a> JitFunc<'a> {
     fn func(&mut self) {
-        self.ctx.func.signature.params = self
-            .f
-            .typ
-            .params
-            .iter()
-            .map(|t| {
-                let Type::Builtin(t) = *t else { todo!() };
-                AbiParam::new(t.into())
-            })
-            .collect();
-        let Type::Builtin(t) = self.f.typ.ret else {
-            todo!()
-        };
-        self.ctx.func.signature.returns = vec![AbiParam::new(t.into())];
+        self.f.typ.to_signature(&mut self.jit.ctx.func.signature);
 
-        let mut builder = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder);
+        let mut builder = FunctionBuilder::new(&mut self.jit.ctx.func, &mut self.ctx);
 
         let entry = builder.create_block();
         builder.append_block_params_for_function_params(entry);
@@ -75,7 +61,7 @@ impl<'a> JitFunc<'a> {
         builder.seal_block(entry);
     }
 
-    fn expr(builder: &mut FunctionBuilder, expr: &Expr) -> Value {
+    fn expr(&mut self, builder: &mut FunctionBuilder, expr: &Expr) -> Value {
         match expr {
             Expr::Ident(ident) => {
                 let Ident::Idx(idx) = ident else { todo!() };
@@ -86,7 +72,29 @@ impl<'a> JitFunc<'a> {
             Expr::Number(n) => builder.ins().f64const(*n),
             Expr::String(..) => todo!(),
             Expr::Boolean(b) => builder.ins().iconst(I8, *b as i64),
-            Expr::Call(..) => todo!(),
+            Expr::Call(f, args) => {
+                let mut sig = self.jit.m.make_signature();
+
+                let Expr::Ident(ident) = &f.item else {
+                    unreachable!();
+                };
+                let id = ident.as_id();
+                self.jit.fns.get(id).unwrap().typ.to_signature(&mut sig);
+                // TODO: How to call back to functions in interpretation mode, or we don't?
+                let callee = self
+                    .jit
+                    .m
+                    .declare_function(&id.raw(), Linkage::Import, &sig)
+                    .unwrap();
+                let local_callee = self.jit.m.declare_func_in_func(callee, builder.func);
+
+                let args = args
+                    .iter()
+                    .map(|a| self.expr(builder, &a.item))
+                    .collect::<Box<_>>();
+                let call = builder.ins().call(local_callee, &args);
+                builder.inst_results(call)[0]
+            }
             Expr::BinaryOp(..) => todo!(),
         }
     }
@@ -103,5 +111,20 @@ impl From<BuiltinType> for JitType {
             BuiltinType::F64 => F64,
             _ => todo!(),
         }
+    }
+}
+
+impl FunctionType {
+    fn to_signature(&self, sig: &mut Signature) {
+        sig.params = self
+            .params
+            .iter()
+            .map(|t| {
+                let Type::Builtin(t) = *t else { todo!() };
+                AbiParam::new(t.into())
+            })
+            .collect();
+        let Type::Builtin(t) = self.ret else { todo!() };
+        sig.returns = vec![AbiParam::new(t.into())];
     }
 }
