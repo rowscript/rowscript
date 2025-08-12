86d54bad0c8d8330340b554e6caa915afd5e17eb
Anqur Lu <anqurvanillapy@gmail.com>
2025-08-12 16:55:26 +0800

    Nits
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

--- crates/core/src/semantics/jit.rs
+++ crates/core/src/semantics/jit.rs
@@ -1,13 +1,16 @@
 use cranelift::codegen::Context;
 use cranelift::prelude::settings::{Flags, builder as flags_builder};
 use cranelift::prelude::types::{F32, F64, I8, I16, I32, I64};
-use cranelift::prelude::{AbiParam, Configurable, FunctionBuilderContext, Type as JitType};
+use cranelift::prelude::{
+    AbiParam, Configurable, FunctionBuilder, FunctionBuilderContext, InstBuilder, Type as JitType,
+    Value, Variable,
+};
 use cranelift_jit::{JITBuilder, JITModule};
 use cranelift_module::{Module, default_libcall_names};
 use cranelift_native::builder as native_builder;
 
 use crate::semantics::{BuiltinType, Func, Type};
-use crate::syntax::Id;
+use crate::syntax::{Expr, Id, Ident};
 
 struct Jit {
     m: JITModule,
@@ -48,7 +51,7 @@ struct JitFunc<'a> {
 }
 
 impl<'a> JitFunc<'a> {
-    fn lower(&mut self) {
+    fn func(&mut self) {
         self.ctx.func.signature.params = self
             .f
             .typ
@@ -63,6 +66,29 @@ impl<'a> JitFunc<'a> {
             todo!()
         };
         self.ctx.func.signature.returns = vec![AbiParam::new(t.into())];
+
+        let mut builder = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder);
+
+        let entry = builder.create_block();
+        builder.append_block_params_for_function_params(entry);
+        builder.switch_to_block(entry);
+        builder.seal_block(entry);
+    }
+
+    fn expr(builder: &mut FunctionBuilder, expr: &Expr) -> Value {
+        match expr {
+            Expr::Ident(ident) => {
+                let Ident::Idx(idx) = ident else { todo!() };
+                builder.use_var(Variable::from_u32(*idx as _))
+            }
+            Expr::BuiltinType(..) => unreachable!(),
+            Expr::Unit => builder.ins().iconst(I8, 0),
+            Expr::Number(n) => builder.ins().f64const(*n),
+            Expr::String(..) => todo!(),
+            Expr::Boolean(b) => builder.ins().iconst(I8, *b as i64),
+            Expr::Call(..) => todo!(),
+            Expr::BinaryOp(..) => todo!(),
+        }
     }
 }
 
