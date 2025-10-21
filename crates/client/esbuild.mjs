b5dadcddfb7d349859f512ba62006aa34c63c1c7
Anqur Lu <anqurvanillapy@gmail.com>
2025-10-21 16:12:43 +0800

    Initialize VS Code extension
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

---
+++ crates/client/esbuild.mjs
@@ -0,0 +1,58 @@
+"use strict";
+
+import { context } from "esbuild";
+
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
+
+async function main() {
+  const ctx = await context({
+    entryPoints: ["extension.ts"],
+    define: {
+      "process.env.NODE_ENV": production ? '"production"' : '"development"',
+    },
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    outfile: "dist/extension.js",
+    external: ["vscode"],
+    logLevel: "warning",
+    plugins: [esbuildProblemMatcherPlugin],
+  });
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
+}
+
+/**
+ * @type {import("esbuild").Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        if (location == null) return;
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`,
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
+};
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
---
+++ crates/client/extension.ts
@@ -0,0 +1,6 @@
+"use strict";
+
+import type { ExtensionContext } from "vscode";
+
+// noinspection JSUnusedGlobalSymbols
+export async function activate(_ctx: ExtensionContext) {}
