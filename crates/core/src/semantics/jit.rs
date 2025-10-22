f9fda55f74975b49235160789bbf231bfd4a394c
Anqur Lu <anqurvanillapy@gmail.com>
2025-10-22 17:46:51 +0800

    More JIT stuff
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

--- crates/core/src/semantics/jit.rs
+++ crates/core/src/semantics/jit.rs
@@ -12,13 +12,12 @@ use cranelift_native::builder as native_builder;
 
 use crate::semantics::{BuiltinType, Func, FunctionType, Functions, Type};
 use crate::syntax::parse::Sym;
-use crate::syntax::{Branch, Expr, Id, Ident, Stmt};
+use crate::syntax::{Branch, Expr, Ident, Stmt};
 use crate::{Span, Spanned};
 
 struct Jit<'a> {
     fns: &'a Functions,
     m: JITModule,
-    ctx: Context,
 }
 
 impl<'a> Jit<'a> {
@@ -30,42 +29,33 @@ impl<'a> Jit<'a> {
             native_builder().unwrap().finish(Flags::new(flags)).unwrap(),
             default_libcall_names(),
         ));
-        let ctx = m.make_context();
-        Self { fns, m, ctx }
+        Self { fns, m }
     }
+}
 
-    fn func(&'a mut self, id: &'a Id, f: &'a Func) -> JitFunc<'a> {
-        JitFunc {
-            jit: self,
-            id,
-            f,
-            ctx: Default::default(),
+impl Func {
+    fn compile(&self, jit: &mut Jit, ctx: &mut Context) {
+        self.typ.to_signature(&mut ctx.func.signature);
+
+        let mut builder_ctx = FunctionBuilderContext::default();
+        let mut builder = FunctionBuilder::new(&mut ctx.func, &mut builder_ctx);
+
+        let entry_block = builder.create_block();
+        builder.append_block_params_for_function_params(entry_block);
+        builder.switch_to_block(entry_block);
+        builder.seal_block(entry_block);
+
+        for s in &self.body {
+            s.item.compile(jit, &mut builder);
         }
+
+        todo!()
     }
 }
 
-struct JitFunc<'a> {
-    jit: &'a mut Jit<'a>,
-    id: &'a Id,
-    f: &'a Func,
-
-    ctx: FunctionBuilderContext,
-}
-
-impl<'a> JitFunc<'a> {
-    fn func(&mut self) {
-        self.f.typ.to_signature(&mut self.jit.ctx.func.signature);
-
-        let mut builder = FunctionBuilder::new(&mut self.jit.ctx.func, &mut self.ctx);
-
-        let entry = builder.create_block();
-        builder.append_block_params_for_function_params(entry);
-        builder.switch_to_block(entry);
-        builder.seal_block(entry);
-    }
-
-    fn expr(&mut self, builder: &mut FunctionBuilder, expr: &Expr) -> Value {
-        match expr {
+impl Expr {
+    fn compile(&self, jit: &mut Jit, builder: &mut FunctionBuilder) -> Value {
+        match self {
             Expr::Ident(ident) => {
                 let Ident::Idx(idx) = ident else { todo!() };
                 builder.use_var(Variable::from_u32(*idx as _))
@@ -76,31 +66,30 @@ impl<'a> JitFunc<'a> {
             Expr::String(..) => todo!("use UstrSet to prevent duplicate in data sections"),
             Expr::Boolean(b) => builder.ins().iconst(I8, *b as i64),
             Expr::Call(f, args) => {
-                let mut sig = self.jit.m.make_signature();
+                let mut sig = jit.m.make_signature();
 
                 let Expr::Ident(ident) = &f.item else {
                     unreachable!();
                 };
                 let id = ident.as_id();
-                self.jit.fns.get(id).unwrap().typ.to_signature(&mut sig);
+                jit.fns.get(id).unwrap().typ.to_signature(&mut sig);
                 // TODO: How to call back to functions in interpretation mode, or we don't?
-                let callee = self
-                    .jit
+                let callee = jit
                     .m
                     .declare_function(&id.raw(), Linkage::Import, &sig)
                     .unwrap();
-                let local_callee = self.jit.m.declare_func_in_func(callee, builder.func);
+                let local_callee = jit.m.declare_func_in_func(callee, builder.func);
 
                 let args = args
                     .iter()
-                    .map(|a| self.expr(builder, &a.item))
+                    .map(|a| a.item.compile(jit, builder))
                     .collect::<Box<_>>();
                 let call = builder.ins().call(local_callee, &args);
                 builder.inst_results(call)[0]
             }
             Expr::BinaryOp(lhs, op, rhs) => {
-                let a = self.expr(builder, &lhs.item);
-                let b = self.expr(builder, &rhs.item);
+                let a = lhs.item.compile(jit, builder);
+                let b = rhs.item.compile(jit, builder);
                 // TODO: Integers.
                 match op {
                     Sym::Le => builder.ins().fcmp(FloatCC::LessThanOrEqual, a, b),
@@ -116,38 +105,63 @@ impl<'a> JitFunc<'a> {
             }
         }
     }
+}
 
-    fn stmt(&mut self, builder: &mut FunctionBuilder, stmt: &Stmt) -> Value {
-        match stmt {
+impl Stmt {
+    fn compile(&self, jit: &mut Jit, builder: &mut FunctionBuilder) -> Value {
+        match self {
             Stmt::Func { .. } => todo!("nested function"),
-            Stmt::Expr(e) => self.expr(builder, e),
+            Stmt::Expr(e) => e.compile(jit, builder),
             Stmt::Assign { name, rhs, .. } | Stmt::Update { name, rhs } => {
                 let Ident::Idx(idx) = name.item else { todo!() };
-                let value = self.expr(builder, &rhs.item);
+                let value = rhs.item.compile(jit, builder);
                 builder.def_var(Variable::from_u32(idx as _), value);
                 value
             }
             Stmt::Return(e) => {
                 let value = e
                     .as_ref()
-                    .map(|v| self.expr(builder, &v.item))
+                    .map(|v| v.item.compile(jit, builder))
                     .unwrap_or_else(|| builder.ins().f64const(0.));
                 builder.ins().return_(&[value]);
                 value
             }
-            Stmt::If { then, elif, els } => self.if_stmt(builder, then, elif, els),
-            Stmt::While(..) => todo!("while statement"),
+            Stmt::If { then, elif, els } => Self::r#if(then, elif, els, jit, builder),
+            Stmt::While(b) => {
+                let header_block = builder.create_block();
+                let body_block = builder.create_block();
+                let exit_block = builder.create_block();
+
+                builder.ins().jump(header_block, &[]);
+                builder.switch_to_block(header_block);
+
+                let cond = b.cond.item.compile(jit, builder);
+                builder.ins().brif(cond, body_block, &[], exit_block, &[]);
+
+                builder.switch_to_block(body_block);
+                builder.seal_block(body_block);
+                for stmt in &b.body {
+                    stmt.item.compile(jit, builder);
+                }
+                builder.ins().jump(header_block, &[]);
+
+                builder.switch_to_block(exit_block);
+                builder.seal_block(header_block);
+                builder.seal_block(exit_block);
+
+                builder.ins().f64const(0.)
+            }
         }
     }
 
-    fn if_stmt(
-        &mut self,
-        builder: &mut FunctionBuilder,
+    fn r#if(
         then: &Branch,
         elif: &[Branch],
-        els: &Option<(Span, Box<[Spanned<Stmt>]>)>,
+        els: &Option<(Span, Box<[Spanned<Self>]>)>,
+        jit: &mut Jit,
+        builder: &mut FunctionBuilder,
     ) -> Value {
-        let cond = self.expr(builder, &then.cond.item);
+        let cond = then.cond.item.compile(jit, builder);
 
         let then_block = builder.create_block();
         let merge_block = builder.create_block();
@@ -167,7 +181,7 @@ impl<'a> JitFunc<'a> {
             builder.seal_block(then_block);
             let mut then_return = builder.ins().f64const(0.);
             for stmt in &then.body {
-                then_return = self.stmt(builder, &stmt.item);
+                then_return = stmt.item.compile(jit, builder);
             }
             builder
                 .ins()
@@ -181,10 +195,10 @@ impl<'a> JitFunc<'a> {
             let mut else_return = builder.ins().f64const(0.);
             if elif.is_empty() {
                 for stmt in &els.as_ref().unwrap().1 {
-                    else_return = self.stmt(builder, &stmt.item);
+                    else_return = stmt.item.compile(jit, builder);
                 }
             } else {
-                else_return = self.if_stmt(builder, &elif[0], &elif[1..], els);
+                else_return = Self::r#if(&elif[0], &elif[1..], els, jit, builder);
             }
 
             builder
