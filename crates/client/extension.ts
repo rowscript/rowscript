ab1869e15a0b553fd0a4e1a01fc2fff03434438f
Anqur Lu <anqurvanillapy@gmail.com>
2025-11-11 10:20:24 +0800

    Nits
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

--- crates/client/extension.ts
+++ crates/client/extension.ts
@@ -22,20 +22,22 @@ function commandHost() {
   }
 }
 
+function command(extensionPath: string) {
+  return isProd ? path.join(extensionPath, commandHost(), id) : id;
+}
+
 // noinspection JSUnusedGlobalSymbols
 export async function activate(ctx: ExtensionContext) {
-  const command = isProd ? path.join(ctx.extensionPath, commandHost(), id) : id;
-  const run = { command, args: ["server"] };
-  const serverOptions = {
-    run,
-    debug: run,
-  };
-  const language = id;
-  const clientOptions = {
-    documentSelector: [{ scheme: "file", language }],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/*.rows"),
+  const run = { command: command(ctx.extensionPath), args: ["server"] };
+  await new LanguageClient(
+    id,
+    name,
+    { run, debug: run },
+    {
+      documentSelector: [{ scheme: "file", language: id }],
+      synchronize: {
+        fileEvents: workspace.createFileSystemWatcher("**/*.rows"),
+      },
     },
-  };
-  await new LanguageClient(id, name, serverOptions, clientOptions).start();
+  ).start();
 }
