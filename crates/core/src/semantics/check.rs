04d13dacb734c76d23dcbf9fdcd2a25b25980335
Anqur Lu <anqurvanillapy@gmail.com>
2025-12-02 17:44:29 +0800

    Utility structure `Kind`
    Signed-off-by: Anqur Lu <anqurvanillapy@gmail.com>

--- crates/core/src/semantics/check.rs
+++ crates/core/src/semantics/check.rs
@@ -10,7 +10,7 @@ use crate::{Error, Out, Span, Spanned};
 
 #[derive(Default)]
 pub(crate) struct Checker {
-    globals: HashMap<Id, (Option<Type>, Type)>,
+    globals: HashMap<Id, Kind>,
     locals: Vec<Type>,
     gs: Globals,
 }
@@ -47,12 +47,12 @@ impl Checker {
                         isa(span, &got, &Type::main())?;
                     }
                     funcs.push(f);
-                    (None, got)
+                    Kind::ValueLevel(got)
                 }
                 Sig::Static { typ } => {
                     let t = self.check_type(typ.span, &mut typ.item)?;
                     statics.push(t.clone());
-                    (None, t)
+                    Kind::ValueLevel(t)
                 }
                 Sig::Struct { members } => {
                     let members = members
@@ -64,10 +64,7 @@ impl Checker {
                         })
                         .collect::<Out<_>>()?;
                     self.gs.structs.insert(name.clone(), Struct { members });
-                    (
-                        Some(Type::Struct(name.clone())),
-                        Type::Builtin(BuiltinType::Type),
-                    )
+                    Kind::TypeLevel(Type::Struct(name.clone()))
                 }
             };
             self.globals.insert(name.clone(), t);
@@ -124,7 +121,7 @@ impl Checker {
 
     fn stmt(&mut self, block: &mut Block, stmt: &mut Spanned<Stmt>) -> Out<Type> {
         Ok(match &mut stmt.item {
-            Stmt::Expr(e) => self.infer(stmt.span, e)?.1,
+            Stmt::Expr(e) => self.infer(stmt.span, e)?.value_level(),
             Stmt::Assign { name, typ, rhs, .. } => {
                 let rhs_type = typ
                     .as_mut()
@@ -133,7 +130,7 @@ impl Checker {
                         self.check(rhs.span, &mut rhs.item, &want)?;
                         Ok(want)
                     })
-                    .unwrap_or_else(|| Ok(self.infer(rhs.span, &mut rhs.item)?.1))?;
+                    .unwrap_or_else(|| Ok(self.infer(rhs.span, &mut rhs.item)?.value_level()))?;
                 self.insert(block, name.item.as_idx(), rhs_type.clone());
                 *typ = Some(Spanned {
                     span: typ.as_ref().map(|t| t.span).unwrap_or(name.span),
@@ -142,7 +139,7 @@ impl Checker {
                 rhs_type
             }
             Stmt::Update { name, rhs } => {
-                let t = self.ident(&name.item)?.1;
+                let t = self.ident(&name.item)?.value_level();
                 self.check(rhs.span, &mut rhs.item, &t)?;
                 t
             }
@@ -195,14 +192,14 @@ impl Checker {
         self.block(block.local(), &mut branch.body)
     }
 
-    fn check(&mut self, span: Span, expr: &mut Expr, want: &Type) -> Out<Option<Type>> {
+    fn check(&mut self, span: Span, expr: &mut Expr, want: &Type) -> Out<Kind> {
         if let Expr::Integer(Integer::I64(n)) = expr
             && let Type::Builtin(t) = want
             && t.is_integer()
             && let Some(n) = t.narrow_integer(*n)
         {
             *expr = Expr::Integer(n);
-            return Ok(None);
+            return Ok(Kind::ValueLevel(want.clone()));
         }
 
         if let Expr::Float(Float::F64(n)) = expr
@@ -210,148 +207,144 @@ impl Checker {
             && t.is_float()
         {
             *expr = Expr::Float(t.narrow_float(*n));
-            return Ok(None);
+            return Ok(Kind::ValueLevel(want.clone()));
         }
 
-        let (typ, got) = self.infer(span, expr)?;
-        isa(span, &got, want)?;
-        Ok(typ)
+        let inferred = self.infer(span, expr)?;
+        let got = match &inferred {
+            Kind::ValueLevel(got) => got,
+            Kind::TypeLevel(..) => &Type::Builtin(BuiltinType::Type),
+        };
+        isa(span, got, want)?;
+        Ok(inferred)
     }
 
     fn check_type(&mut self, span: Span, expr: &mut Expr) -> Out<Type> {
         self.check(span, expr, &Type::Builtin(BuiltinType::Type))
-            .map(Option::unwrap)
+            .map(Kind::type_level)
     }
 
-    fn infer(&mut self, span: Span, expr: &mut Expr) -> Out<(Option<Type>, Type)> {
-        Ok((
-            None,
-            match expr {
-                Expr::Ident(ident) => return self.ident(ident),
-                Expr::BuiltinType(t) => {
-                    return Ok((Some(Type::Builtin(*t)), Type::Builtin(BuiltinType::Type)));
-                }
-                Expr::RefType(t) => {
-                    let t = self.check_type(t.span, &mut t.item)?;
-                    return Ok((
-                        Some(Type::Ref(Box::new(t))),
-                        Type::Builtin(BuiltinType::Type),
-                    ));
-                }
-                Expr::Unit => Type::Builtin(BuiltinType::Unit),
-                Expr::Integer(n) => {
-                    debug_assert!(matches!(n, Integer::I64(..)));
-                    Type::Builtin(BuiltinType::I64)
-                }
-                Expr::Float(n) => {
-                    debug_assert!(matches!(n, Float::F64(..)));
-                    Type::Builtin(BuiltinType::F64)
-                }
-                Expr::String(..) => Type::Builtin(BuiltinType::Str),
-                Expr::Boolean(..) => Type::Builtin(BuiltinType::Bool),
-                Expr::Call(f, args) => {
-                    let typ = self.infer(f.span, &mut f.item)?.1;
-                    let Type::Function(typ) = typ else {
-                        return Err(Error::TypeMismatch {
-                            span,
-                            got: typ.to_string(),
-                            want: "function".to_string(),
-                        });
-                    };
-                    {
-                        let got = args.len();
-                        let want = typ.params.len();
-                        if got != want {
-                            return Err(Error::ArityMismatch { span, got, want });
-                        }
+    fn infer(&mut self, span: Span, expr: &mut Expr) -> Out<Kind> {
+        Ok(Kind::ValueLevel(match expr {
+            Expr::Ident(ident) => return self.ident(ident),
+            Expr::BuiltinType(t) => return Ok(Kind::TypeLevel(Type::Builtin(*t))),
+            Expr::RefType(t) => {
+                let t = self.check_type(t.span, &mut t.item)?;
+                return Ok(Kind::TypeLevel(Type::Ref(Box::new(t))));
+            }
+            Expr::Unit => Type::Builtin(BuiltinType::Unit),
+            Expr::Integer(n) => {
+                debug_assert!(matches!(n, Integer::I64(..)));
+                Type::Builtin(BuiltinType::I64)
+            }
+            Expr::Float(n) => {
+                debug_assert!(matches!(n, Float::F64(..)));
+                Type::Builtin(BuiltinType::F64)
+            }
+            Expr::String(..) => Type::Builtin(BuiltinType::Str),
+            Expr::Boolean(..) => Type::Builtin(BuiltinType::Bool),
+            Expr::Call(f, args) => {
+                let typ = self.infer(f.span, &mut f.item)?.value_level();
+                let Type::Function(typ) = typ else {
+                    return Err(Error::TypeMismatch {
+                        span,
+                        got: typ.to_string(),
+                        want: "function".to_string(),
+                    });
+                };
+                {
+                    let got = args.len();
+                    let want = typ.params.len();
+                    if got != want {
+                        return Err(Error::ArityMismatch { span, got, want });
                     }
-                    args.iter_mut()
-                        .zip(typ.params.iter())
-                        .try_for_each(|(got, want)| {
-                            self.check(got.span, &mut got.item, want)?;
-                            Ok(())
-                        })?;
-                    typ.ret
                 }
-                Expr::BinaryOp(lhs, op, typ, rhs) => match op {
-                    Sym::EqEq => {
-                        let got = self.infer(lhs.span, &mut lhs.item)?.1;
+                args.iter_mut()
+                    .zip(typ.params.iter())
+                    .try_for_each(|(got, want)| {
+                        self.check(got.span, &mut got.item, want)?;
+                        Ok(())
+                    })?;
+                typ.ret
+            }
+            Expr::BinaryOp(lhs, op, typ, rhs) => match op {
+                Sym::EqEq => {
+                    let got = self.infer(lhs.span, &mut lhs.item)?.value_level();
+                    self.check(rhs.span, &mut rhs.item, &got)?;
+                    *typ = Some(got);
+                    Type::Builtin(BuiltinType::Bool)
+                }
+                Sym::Lt | Sym::Gt | Sym::Le | Sym::Ge => {
+                    let got = self.infer(lhs.span, &mut lhs.item)?.value_level();
+                    if let Type::Builtin(t) = got
+                        && (t.is_integer() || t.is_float())
+                    {
                         self.check(rhs.span, &mut rhs.item, &got)?;
                         *typ = Some(got);
                         Type::Builtin(BuiltinType::Bool)
+                    } else {
+                        return Err(Error::TypeMismatch {
+                            span: lhs.span,
+                            got: got.to_string(),
+                            want: "number".to_string(),
+                        });
                     }
-                    Sym::Lt | Sym::Gt | Sym::Le | Sym::Ge => {
-                        let got = self.infer(lhs.span, &mut lhs.item)?.1;
-                        if let Type::Builtin(t) = got
-                            && (t.is_integer() || t.is_float())
-                        {
-                            self.check(rhs.span, &mut rhs.item, &got)?;
-                            *typ = Some(got);
-                            Type::Builtin(BuiltinType::Bool)
-                        } else {
-                            return Err(Error::TypeMismatch {
-                                span: lhs.span,
-                                got: got.to_string(),
-                                want: "number".to_string(),
-                            });
-                        }
-                    }
-                    Sym::Plus | Sym::Minus | Sym::Mul => {
-                        let got = self.infer(lhs.span, &mut lhs.item)?.1;
-                        if let Type::Builtin(t) = got
-                            && (t.is_integer() || t.is_float())
-                        {
-                            self.check(rhs.span, &mut rhs.item, &got)?;
-                            *typ = Some(got.clone());
-                            got
-                        } else {
-                            return Err(Error::TypeMismatch {
-                                span: lhs.span,
-                                got: got.to_string(),
-                                want: "number".to_string(),
-                            });
-                        }
-                    }
-                    _ => unreachable!(),
-                },
-                Expr::UnaryOp(x, op, typ) => match op {
-                    Sym::Mul => {
-                        let got = self.infer(x.span, &mut x.item)?.1;
-                        let Type::Ref(got) = got else {
-                            return Err(Error::TypeMismatch {
-                                span,
-                                got: got.to_string(),
-                                want: "reference type".to_string(),
-                            });
-                        };
-                        *typ = Some(*got.clone());
-                        *got
-                    }
-                    _ => unreachable!(),
-                },
-                Expr::New(t) => {
-                    let typ = self.check_type(t.span, &mut t.item)?;
-                    Type::Function(Box::new(FunctionType {
-                        params: Box::new([typ.clone()]), // TODO: accurate arity
-                        ret: Type::Ref(Box::new(typ)),
-                    }))
                 }
-                Expr::Initialize(t, args) => {
-                    self.check_type(t.span, &mut t.item)?;
-                    _ = args;
-                    todo!()
+                Sym::Plus | Sym::Minus | Sym::Mul => {
+                    let got = self.infer(lhs.span, &mut lhs.item)?.value_level();
+                    if let Type::Builtin(t) = got
+                        && (t.is_integer() || t.is_float())
+                    {
+                        self.check(rhs.span, &mut rhs.item, &got)?;
+                        *typ = Some(got.clone());
+                        got
+                    } else {
+                        return Err(Error::TypeMismatch {
+                            span: lhs.span,
+                            got: got.to_string(),
+                            want: "number".to_string(),
+                        });
+                    }
                 }
-                Expr::Access(..) => todo!("access"),
-                Expr::Ref(..) => unreachable!(),
+                _ => unreachable!(),
             },
-        ))
+            Expr::UnaryOp(x, op, typ) => match op {
+                Sym::Mul => {
+                    let got = self.infer(x.span, &mut x.item)?.value_level();
+                    let Type::Ref(got) = got else {
+                        return Err(Error::TypeMismatch {
+                            span,
+                            got: got.to_string(),
+                            want: "reference type".to_string(),
+                        });
+                    };
+                    *typ = Some(*got.clone());
+                    *got
+                }
+                _ => unreachable!(),
+            },
+            Expr::New(t) => {
+                let typ = self.check_type(t.span, &mut t.item)?;
+                Type::Function(Box::new(FunctionType {
+                    params: Box::new([typ.clone()]), // TODO: accurate arity
+                    ret: Type::Ref(Box::new(typ)),
+                }))
+            }
+            Expr::Initialize(t, args) => {
+                self.check_type(t.span, &mut t.item)?;
+                _ = args;
+                todo!()
+            }
+            Expr::Access(..) => todo!("access"),
+            Expr::Ref(..) => unreachable!(),
+        }))
     }
 
-    fn ident(&self, ident: &Ident) -> Out<(Option<Type>, Type)> {
+    fn ident(&self, ident: &Ident) -> Out<Kind> {
         Ok(match ident {
             Ident::Id(n) => self.globals.get(n).unwrap().clone(),
-            Ident::Idx(i) => (None, self.locals.get(*i).unwrap().clone()),
-            Ident::Builtin(b) => (None, b.r#type()),
+            Ident::Idx(i) => Kind::ValueLevel(self.locals.get(*i).unwrap().clone()),
+            Ident::Builtin(b) => Kind::ValueLevel(b.r#type()),
         })
     }
 
@@ -376,6 +369,30 @@ impl Block {
             ret: self.ret.clone(),
             locals: 0,
         }
+    }
+}
+
+#[derive(Clone)]
+enum Kind {
+    /// The expression is value-level, e.g. for `⊢ x : T`, the [`Type`] here represents `T`.
+    ValueLevel(Type),
+    /// The expression is type-level, e.g. for `⊢ T type`, the [`Type`] here represents `T`.
+    TypeLevel(Type),
+}
+
+impl Kind {
+    fn value_level(self) -> Type {
+        if let Self::ValueLevel(typ) = self {
+            return typ;
+        }
+        unreachable!()
+    }
+
+    fn type_level(self) -> Type {
+        if let Self::TypeLevel(typ) = self {
+            return typ;
+        }
+        unreachable!()
     }
 }
 
